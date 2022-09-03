e9cd4ab8622628a676941357822f468c9ae5a61e
sayaliranaware <58587643+sayaliranaware@users.noreply.github.com>
2022-09-03 13:29:34 +0530

    Update Owasp-Dependency-Check.sh

--- Owasp-Dependency-Check.sh
+++ Owasp-Dependency-Check.sh
@@ -18,7 +18,7 @@ docker pull owasp/dependency-check
 
 docker run --rm \
     --volume $(pwd):/src \
-    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data \
+    --volume "$DATA_DIRECTORY":/var/lib/jenkins/OWASP-Dependency-Check/data/ \
     --volume "$REPORT_DIRECTORY":/report \
     owasp/dependency-check \
     --scan /src \
